7f2f8dcbc67c81d4a2e429d10e701733b51639ba
Rene Ivan Limon Chacon <rene.limon@globant.com>
2022-01-20 10:46:06 -0600

    created new env file to manage pwa in dev mode

--- next.config.js
+++ next.config.js
@@ -5,11 +5,13 @@
 
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
+const prod = process.env.NODE_ENV === "production";
 
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
     dest: "public",
+    disable: prod ? false : true,
     runtimeCaching,
   },
 });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,8 @@ import General from "../components/grid/generalTemplate";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Container>
+    <>
+      <Container>
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -34,10 +35,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#317EFB" />
       </Head>
+      {/* <Container> */}
       <General>
         <Component {...pageProps} />
       </General>
     </Container>
+    </>
   );
 }
 
